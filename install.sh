db6694afb6b30d2407fb7608e49f5271277b99a9
Marc Milgrom <mdmilgrom@gmail.com>
2015-11-08 10:27:58 -0700

    update requirements and install for matplotlib

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir ~/machinelearning
+mkdir ~/.matplotlib
 cd ~/machinelearning
 virtualenv -ppython2.7 venv
 source venv/bin/activate
 pip install -r requirements.txt
+echo "backend: TkAgg" > ~/.matplotlib/matplotibrc
